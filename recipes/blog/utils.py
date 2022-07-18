0847558757dbab4ec9550c0d23e24172ecace226
Gobls <91689726+Gobls@users.noreply.github.com>
2022-07-18 22:35:17 +0400

    mistake

--- recipes/blog/utils.py
+++ recipes/blog/utils.py
@@ -2,7 +2,7 @@ from blog.models import Category
 
 menu = [{'title' : "О сайте", 'url_name' : 'about'},
         {'title' : "Добавить статью", 'url_name' : 'add_page'},
-        {'title' : "Обратная связь", 'url_name' : 'contact'},
+        #{'title' : "Обратная связь", 'url_name' : 'contact'},
         # {'title' : "Войти", 'url_name' : 'login'}
         ]
 
